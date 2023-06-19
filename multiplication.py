c1d6448850897f19703ec9ac20fa9691b094681d
THIVYAPRAKASH <134679457+THIVYAPRAKASH@users.noreply.github.com>
2023-06-19 11:13:10 +0530

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -3,6 +3,17 @@ for i in range(1,11):
     m=e*i
     print(e,'X',i,'=',m)
 
- 
+output:
+Enter The Mulitiplication Tabel You Want :7
+7 X 1 = 7
+7 X 2 = 14
+7 X 3 = 21
+7 X 4 = 28
+7 X 5 = 35
+7 X 6 = 42
+7 X 7 = 49
+7 X 8 = 56
+7 X 9 = 63
+7 X 10 = 70
     
 
